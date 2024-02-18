96df1bfe2317e53569cbe9cdf0f92816f33e2293
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 13:01:26 -0600

    In the home page, use the server actions to search and display the recipe(s) from the title entered in the form

--- recipe-app/src/app/page.tsx
+++ recipe-app/src/app/page.tsx
@@ -1,11 +1,40 @@
+'use client';
+
+import { useFormState, useFormStatus } from 'react-dom';
+import { searchRecipesAction } from '@/server/actions';
+import { mockRecipeData } from '@/server/db';
+
+import { Loader2 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
+const SubmitButton = () => {
+	const { pending } = useFormStatus();
+
+	return (
+		<Button
+			className="h-12 w-32"
+			type="submit"
+		>
+			{pending ? <Loader2 className="animate-spin" /> : 'Search'}
+		</Button>
+	);
+};
+
 export default function Home() {
+	const [state, formAction] = useFormState(searchRecipesAction, {
+		title: '',
+		recipes: mockRecipeData, // The initial state is all recipes
+		success: true, // The initial state is successful because we are returning all recipes
+	});
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<section className="flex w-full flex-col items-center gap-8 rounded-xl border border-black p-5">
-				<form className="flex w-3/4 flex-row gap-2">
+				<form
+					action={formAction}
+					className="flex w-3/4 flex-row gap-2"
+				>
 					<Input
 						className="h-12"
 						placeholder="Search for recipes..."
@@ -14,13 +43,25 @@ export default function Home() {
 						autoFocus={true}
 					/>
 
-					<Button
-						className="h-12"
-						type="submit"
-					>
-						Search
-					</Button>
+					<SubmitButton />
 				</form>
+
+				{state.success ? (
+					<article className="grid w-full grid-cols-3 gap-4">
+						{state.recipes.map((recipe) => (
+							<div
+								key={recipe.id}
+								className="rounded-xl border border-black p-4"
+							>
+								<h2 className="text-2xl font-bold">{recipe.title}</h2>
+							</div>
+						))}
+					</article>
+				) : (
+					<p className="text-red-500">
+						No recipes found with the title: {state.title}. Please try again.
+					</p>
+				)}
 			</section>
 		</main>
 	);
