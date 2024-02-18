0189161f09c38835496cea9e920431e2748c4ec8
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 12:39:19 -0600

    Add a basic search form that has no functionality yet

--- recipe-app/src/app/page.tsx
+++ recipe-app/src/app/page.tsx
@@ -1,5 +1,27 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<section className="flex w-full flex-col items-center gap-8 rounded-xl border border-black p-5">
+				<form className="flex w-3/4 flex-row gap-2">
+					<Input
+						className="h-12"
+						placeholder="Search for recipes..."
+						type="search"
+						name="title"
+						autoFocus={true}
+					/>
+
+					<Button
+						className="h-12"
+						type="submit"
+					>
+						Search
+					</Button>
+				</form>
+			</section>
+		</main>
+	);
 }
