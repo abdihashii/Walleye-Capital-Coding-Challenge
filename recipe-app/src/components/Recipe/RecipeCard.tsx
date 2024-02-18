427df795f357cc4d53bde095bdf2464740ac8d05
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 13:10:25 -0600

    Add spacing to recipe card items

--- recipe-app/src/components/Recipe/RecipeCard.tsx
+++ recipe-app/src/components/Recipe/RecipeCard.tsx
@@ -3,22 +3,26 @@ import React from 'react';
 
 const RecipeCard = ({ recipe }: { recipe: TRecipe }) => {
 	return (
-		<div className="flex flex-col rounded-xl border border-black p-4">
+		<div className="flex flex-col gap-8 rounded-xl border border-black p-4">
 			<h2 className="h-28 text-2xl font-bold">{recipe.title}</h2>
 
-			<h3 className="text-lg font-bold">Ingredients</h3>
-			<ul className="h-[72px] list-inside list-disc overflow-y-scroll">
-				{recipe.ingredients.map((ingredient, index) => (
-					<li key={index}>{ingredient}</li>
-				))}
-			</ul>
+			<div>
+				<h3 className="text-lg font-bold">Ingredients</h3>
+				<ul className="h-[72px] list-inside list-disc overflow-y-scroll">
+					{recipe.ingredients.map((ingredient, index) => (
+						<li key={index}>{ingredient}</li>
+					))}
+				</ul>
+			</div>
 
-			<h3 className="text-lg font-bold">Instructions</h3>
-			<ol className="h-[72px] list-inside list-decimal overflow-y-scroll">
-				{recipe.instructions.map((instruction, index) => (
-					<li key={index}>{instruction}</li>
-				))}
-			</ol>
+			<div>
+				<h3 className="text-lg font-bold">Instructions</h3>
+				<ol className="h-[72px] list-inside list-decimal overflow-y-scroll">
+					{recipe.instructions.map((instruction, index) => (
+						<li key={index}>{instruction}</li>
+					))}
+				</ol>
+			</div>
 		</div>
 	);
 };
