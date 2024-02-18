6d0c1528a3eac4d4c4c59a35617307efd38ce09f
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 13:48:50 -0600

    Add an `isPage` prop to the RecipeCard component to show a back arrow in instances on a page.

--- recipe-app/src/components/Recipe/RecipeCard.tsx
+++ recipe-app/src/components/Recipe/RecipeCard.tsx
@@ -1,18 +1,35 @@
 import { TRecipe } from '@/types';
+import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
-const RecipeCard = ({ recipe }: { recipe: TRecipe }) => {
+const RecipeCard = ({
+	recipe,
+	isPage = false,
+}: {
+	recipe: TRecipe;
+	isPage?: boolean;
+}) => {
 	return (
 		<div className="flex flex-col gap-8 rounded-xl border border-black p-4">
-			<h2 className="line-clamp-3 h-28 text-2xl font-bold">
-				<Link
-					href={`/recipe/${recipe.id}`}
-					className="text-blue-600 transition-all duration-300 hover:text-blue-800 hover:underline"
-				>
-					{recipe.title}
-				</Link>
-			</h2>
+			<div className="flex flex-row items-center gap-4">
+				{isPage && (
+					<Link
+						href="/"
+						className="rounded-lg bg-gray-300 p-2 transition-all duration-300 hover:bg-gray-400"
+					>
+						<ArrowLeft className="h-6 w-6" />
+					</Link>
+				)}
+				<h2 className="line-clamp-3 h-28 text-2xl font-bold">
+					<Link
+						href={`/recipe/${recipe.id}`}
+						className="text-blue-600 transition-all duration-300 hover:text-blue-800 hover:underline"
+					>
+						{recipe.title}
+					</Link>
+				</h2>
+			</div>
 
 			<div>
 				<h3 className="text-lg font-bold">Ingredients</h3>
