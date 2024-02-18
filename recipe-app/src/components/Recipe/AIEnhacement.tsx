78b5475b3e392511247bea8617cfeec6c260f902
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 14:33:53 -0600

    Now we're using tanstack query to handle our fetching for the AI response!

--- recipe-app/src/components/Recipe/AIEnhacement.tsx
+++ recipe-app/src/components/Recipe/AIEnhacement.tsx
@@ -1,50 +1,68 @@
 'use client';
 
 import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 import { Button } from '../ui/button';
 import { Loader2 } from 'lucide-react';
 
 const AIEnhacement = ({ title }: { title: string }) => {
-	const [history, setHistory] = useState<string | null>(null);
-	const [loading, setLoading] = useState(false);
+	// A state to make sure we only fetch the history on button click
+	const [fetchEnabled, setFetchEnabled] = useState(false);
 
 	const fetchHistory = async () => {
-		setLoading(true);
+		const response = await fetch('/api/ai/getHistory', {
+			method: 'POST',
+			body: JSON.stringify({ title }),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
 
-		try {
-			const response = await fetch('/api/ai/getHistory', {
-				method: 'POST',
-				body: JSON.stringify({ title }),
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			});
-
-			if (!response.ok) {
-				throw new Error('Network response was not ok');
-			}
-
-			const json = await response.json();
-
-			setHistory(json.data);
-		} catch (error) {
-			console.error(error);
-		} finally {
-			setLoading(false);
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
 		}
+
+		const json: {
+			data: string;
+		} = await response.json();
+
+		return json;
+	};
+
+	const {
+		data: history,
+		error,
+		isLoading,
+	} = useQuery({
+		queryKey: ['history', title], // Including the title because there are a lot of recipes
+		queryFn: fetchHistory,
+		enabled: fetchEnabled,
+	});
+
+	if (error) {
+		return (
+			<div>
+				<p className="text-red-500">Error: {error.message}</p>
+			</div>
+		);
+	}
+
+	const handleGetHistory = () => {
+		setFetchEnabled(true);
 	};
 
 	return (
 		<div className="flex w-full flex-col items-center gap-4">
 			<Button
 				className="w-32"
-				onClick={fetchHistory}
+				onClick={handleGetHistory}
+				disabled={isLoading || history ? true : false}
 			>
-				{loading ? <Loader2 className="animate-spin" /> : 'Get History'}
+				{isLoading ? <Loader2 className="animate-spin" /> : 'Get History'}
 			</Button>
 
-			{history && <p>{history}</p>}
+			{history && <p>{history.data}</p>}
 		</div>
 	);
 };
