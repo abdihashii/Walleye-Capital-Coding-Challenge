fd82daf423cc44497f6657e1d47c491861b24ec5
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 13:43:01 -0600

    Add a back button to go back to the home page

--- recipe-app/src/app/recipe/[id]/page.tsx
+++ recipe-app/src/app/recipe/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { getRecipeFromId } from '@/lib/utils';
 import React from 'react';
 
+import Link from 'next/link';
+import { ArrowLeft } from 'lucide-react';
+
 export default async function RecipePage({
 	params,
 }: {
@@ -26,7 +29,15 @@ export default async function RecipePage({
 		<main className="flex min-h-screen flex-col items-center justify-center p-24">
 			<section className="flex w-full justify-center rounded-xl border border-black p-5 2xl:w-3/4">
 				<div className="flex w-fit flex-col gap-8">
-					<h1 className="text-4xl font-bold text-blue-700">{recipe.title}</h1>
+					<div className="flex flex-row items-center gap-4">
+						<Link
+							href="/"
+							className="rounded-lg bg-gray-300 p-2 transition-all duration-300 hover:bg-gray-400"
+						>
+							<ArrowLeft className="h-6 w-6" />
+						</Link>
+						<h1 className="text-4xl font-bold text-blue-700">{recipe.title}</h1>
+					</div>
 
 					<div>
 						<h2 className="text-2xl font-bold">Ingredients</h2>
