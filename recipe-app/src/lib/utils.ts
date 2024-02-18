e23b55f4144be201290d835c71ccec5e63536d18
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 13:34:42 -0600

    Add a getRecipeFromId method that will hep us query the database and get a recipe by its ID. Also adding JSDoc comments

--- recipe-app/src/lib/utils.ts
+++ recipe-app/src/lib/utils.ts
@@ -8,7 +8,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 /**
- * This function searches the recipe database for a recipe with the given title.
+ * This function searches the recipe database for a recipe with the given
+ * title. I'm making this return a promise to simulate an async operation to a
+ * real database. We currently have a mock database, so this is not necessary.
+ * It's just to show how we would handle an async operation.
  * @param title - The title of the recipe to search for in the database
  * @returns - A promise that resolves when the search is complete with the
  * title and an array of recipes that match the title. The promise will reject
@@ -33,4 +36,28 @@ export function getRecipesFromTitle(title: string): Promise<{
 	}
 
 	return Promise.resolve({ title, recipes: filteredRecipes });
+}
+
+/**
+ * This function searches the recipe database for a recipe with the given id.
+ * I'm making this return a promise to simulate an async operation to a real
+ * database. We currently have a mock database, so this is not necessary. It's
+ * just to show how we would handle an async operation.
+ * @param id - The id of the recipe to search for in the database
+ * @returns - A promise that resolves when the search is complete with the
+ * recipe that matches the id. The promise will reject if there is an error.
+ */
+export function getRecipeFromId(
+	id: string,
+): Promise<{ error?: string; recipe?: TRecipe }> {
+	// Search for the recipe from the database by the id
+	const recipe = mockRecipeData.find((recipe) => recipe.id === id);
+
+	if (!recipe) {
+		const errorMessage = `Unable to find the recipe with the id: ${id}`;
+
+		return Promise.resolve({ error: errorMessage });
+	}
+
+	return Promise.resolve({ recipe });
 }
