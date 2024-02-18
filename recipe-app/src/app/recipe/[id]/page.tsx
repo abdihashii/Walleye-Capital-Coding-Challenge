3feb08d5d913d0ea02c548c323aba41f096cea13
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 13:22:07 -0600

    Trying to match the <main> element styling from the home page to the /recipe/[id] page

--- recipe-app/src/app/recipe/[id]/page.tsx
+++ recipe-app/src/app/recipe/[id]/page.tsx
@@ -8,7 +8,7 @@ export default async function RecipePage({
 	};
 }) {
 	return (
-		<main>
+		<main className="flex min-h-screen flex-col items-center justify-center p-24">
 			<h1>Recipe page for recipe with id: {params.id}</h1>
 		</main>
 	);
