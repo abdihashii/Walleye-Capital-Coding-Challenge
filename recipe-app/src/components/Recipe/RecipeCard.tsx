57283d2835aed01b56dad74335e052da57c0dee3
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 13:08:31 -0600

    We also need to make sure each ingredient and instructions list are the same height for each recipe card

--- recipe-app/src/components/Recipe/RecipeCard.tsx
+++ recipe-app/src/components/Recipe/RecipeCard.tsx
@@ -7,14 +7,14 @@ const RecipeCard = ({ recipe }: { recipe: TRecipe }) => {
 			<h2 className="h-28 text-2xl font-bold">{recipe.title}</h2>
 
 			<h3 className="text-lg font-bold">Ingredients</h3>
-			<ul className="list-inside list-disc">
+			<ul className="h-[72px] list-inside list-disc overflow-y-scroll">
 				{recipe.ingredients.map((ingredient, index) => (
 					<li key={index}>{ingredient}</li>
 				))}
 			</ul>
 
 			<h3 className="text-lg font-bold">Instructions</h3>
-			<ol className="list-inside list-decimal">
+			<ol className="h-[72px] list-inside list-decimal overflow-y-scroll">
 				{recipe.instructions.map((instruction, index) => (
 					<li key={index}>{instruction}</li>
 				))}
