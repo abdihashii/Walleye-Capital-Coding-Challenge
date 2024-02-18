a72473f891c24de8e15f47a561c2aa7920d3eb57
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 13:40:05 -0600

    Update the recipe page server component to load the recipe data by the id and display it

--- recipe-app/src/app/recipe/[id]/page.tsx
+++ recipe-app/src/app/recipe/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { getRecipeFromId } from '@/lib/utils';
 import React from 'react';
 
 export default async function RecipePage({
@@ -7,9 +8,45 @@ export default async function RecipePage({
 		id: string;
 	};
 }) {
+	const recipeData = await getRecipeFromId(params.id);
+
+	const { error, recipe } = recipeData;
+
+	if (error || !recipe) {
+		return (
+			<main className="flex min-h-screen flex-col items-center justify-center p-24">
+				<section className="w-full flex-col items-center rounded-xl border border-black p-5">
+					<h1>{error}</h1>
+				</section>
+			</main>
+		);
+	}
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center p-24">
-			<h1>Recipe page for recipe with id: {params.id}</h1>
+			<section className="flex w-full justify-center rounded-xl border border-black p-5 2xl:w-3/4">
+				<div className="flex w-fit flex-col gap-8">
+					<h1 className="text-4xl font-bold text-blue-700">{recipe.title}</h1>
+
+					<div>
+						<h2 className="text-2xl font-bold">Ingredients</h2>
+						<ul>
+							{recipe.ingredients.map((ingredient, index) => (
+								<li key={index}>{ingredient}</li>
+							))}
+						</ul>
+					</div>
+
+					<div>
+						<h2 className="text-2xl font-bold">Instructions</h2>
+						<ol>
+							{recipe.instructions.map((instruction, index) => (
+								<li key={index}>{instruction}</li>
+							))}
+						</ol>
+					</div>
+				</div>
+			</section>
 		</main>
 	);
 }
