b39fdd61036de592c1c06b861754a0e06fcb6c4f
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 14:41:00 -0600

    Add support for caching app data

--- recipe-app/src/providers/AppQueryProvider.tsx
+++ recipe-app/src/providers/AppQueryProvider.tsx
@@ -1,10 +1,27 @@
 'use client';
 
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { persistQueryClient } from '@tanstack/react-query-persist-client';
 
 const AppQueryProvider = ({ children }: { children: React.ReactNode }) => {
-	const queryClient = new QueryClient();
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				gcTime: 1000 * 60 * 60 * 24, // Cache the data for 24 hours
+			},
+		},
+	});
+
+	const localStoragePersister = createSyncStoragePersister({
+		storage: window.localStorage,
+	});
+
+	persistQueryClient({
+		queryClient,
+		persister: localStoragePersister,
+	});
 
 	return (
 		<QueryClientProvider client={queryClient}>
