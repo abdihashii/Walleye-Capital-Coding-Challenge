b13b080209300f55f9465b51afbc0a7a5850e38a
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-18 12:41:29 -0600

    Add mock recipe data

---
+++ recipe-app/src/server/db.ts
@@ -0,0 +1,40 @@
+import { TRecipe } from '@/types';
+
+export const mockRecipeData: Array<TRecipe> = [
+	{
+		id: '1',
+		title: 'Basto Iskukaris | Spaghetti Bolognese',
+		ingredients: ['pasta', 'tomato sauce', 'ground beef'],
+		instructions: ['Boil pasta', 'Cook ground beef', 'Mix pasta and beef'],
+	},
+	{
+		id: '2',
+		title: 'Malawax | Somali Crepe',
+		ingredients: ['flour', 'sugar', 'milk'],
+		instructions: ['Mix flour, sugar, and milk', 'Cook the malawax'],
+	},
+	{
+		id: '3',
+		title: 'Bariis iyo hilib ari | Somali Rice and Goat Meat',
+		ingredients: ['rice', 'goat meat', 'xawaash | spices'],
+		instructions: ['Cook rice', 'Cook goat meat', 'Mix rice and goat meat'],
+	},
+	{
+		id: '4',
+		title: 'Canjeero | Somali Pancake',
+		ingredients: ['flour', 'sugar', 'yeast'],
+		instructions: ['Mix flour, sugar, and yeast', 'Cook the canjeero'],
+	},
+	{
+		id: '5',
+		title: 'Sambuuso',
+		ingredients: ['flour', 'ground beef', 'spices'],
+		instructions: ['Mix flour, ground beef, and spices', 'Cook the sambuus'],
+	},
+	{
+		id: '6',
+		title: 'Chicken Suqaar | Cubed Chicken Stir Fry',
+		ingredients: ['chicken', 'onion', 'spices'],
+		instructions: ['Cook chicken', 'Cook onion', 'Mix chicken and onion'],
+	},
+];
